944f5f42600b8d7ca3a2f3a310df9bf4210a8725
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-25 23:42:17 -0500

    added more routes

--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,42 @@ router.get("/hello", (req, res) => {
     res.send({ message: "Start Coding You Lazy Piece of Shit!" });
 });
 
+router.post("/seed", (req, res) => {
+   var UserSeed = [
+        {
+          firstName: "Dion",
+          lastName: "Cavanaugh",
+          email:'dcdeveloper26@gmail.com',
+          password: 'admin',
+          date: new Date(Date.now())
+        },
+        {
+          firstName: "Troy",
+          lastName: "Slaten",
+          email:'dcavanaugh2525@gmail.com',
+          password: 'admin',
+          date: new Date(Date.now())
+        }
+      ];
+
+      db.User
+        .collection.insertMany(UserSeed)
+        .then(data => {
+            console.log(data.insertedIds.length + " records inserted!");
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+});
+router.get("/all", (req, res) => {
+    db.User
+        .find({})
+        .then(data => {res.send(data)})
+        .catch(err => {
+            console.log(err);
+        });
+});
 
 
 module.exports = router;
