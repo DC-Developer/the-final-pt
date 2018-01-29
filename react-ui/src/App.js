d155b16b35a0b5f1abd3b7a32697beefdb486ee0
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-29 00:56:53 -0500

    added basic react component structure

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+ 
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,30 +12,44 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('/api/hello')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
+    // fetch('/api/hello')
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error(`status ${response.status}`);
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(json => {
+    //     this.setState({
+    //       message: json.message,
+    //       fetching: false
+    //     });
+    //   })
+    //   .catch(e => {
+    //     this.setState({
+    //       message: `API call failed: ${e}`,
+    //       fetching: false
+    //     });
+    //   })
+    this.callApi()
+    .then(res => this.setState({message: res.message, fetching: false}))
+    .catch(err => console.log(err));
+
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/hello");
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
   }
 
   render() {
     return (
-      <div className="-Login-Alt">
-          
+      <div className="btn btn-primary btn-lg">
+        {this.state.message}
       </div>
     );
   }
