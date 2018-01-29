2e332ac2582edb88918f13164b526c13b4093163
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-29 04:55:17 -0500

    finished login page

--- react-ui/src/pages/Login/Login.js
+++ react-ui/src/pages/Login/Login.js
@@ -24,6 +24,22 @@ class Login extends React.Component {
                     <div className="logoDiv">
                         <img src={icon.src} className="logo"/>FIT CLIENT
                     </div>
+                    <div className="oauth2">
+                        <h1>Login</h1>
+                        <button className="Rectangle-Copy">Facebook</button>
+                        <button className="Rectangle-Copy-2">Google</button>
+                        
+                        <div className="hrDiv">
+                            <hr /><p className="hrText">or</p><hr />
+                        </div>
+
+                        <form action ="" className="regLog" method="post">
+                            <input type="text" placeholder="    Email" className="Rectangle-login" /><br />
+                            <input type="text" placeholder="    Password" className="Rectangle-login" /><br />
+                            <input type="submit" value="Login" className="Rectangle-Copy-3" />
+                        </form>
+                    </div>
+  
                 </div>
             </div>
         );
