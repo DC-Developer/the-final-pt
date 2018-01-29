5fdd3e4add0303db2020c003fd20f86741a63ca5
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-29 17:16:07 -0500

    added client page component jsx

---
+++ react-ui/src/components/SitePage/SitePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SitePage.css';
+
+//this component will be setting the size for the client page
+
+const SitePage = ({children}) => {
+    return (
+        <div className="parentElement">
+            <div className="childElement">
+            
+            </div>
+        </div>
+    );
+}
+
+export default SitePage;
--- react-ui/src/pages/Client/Client.js
+++ react-ui/src/pages/Client/Client.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ClientPageSize from '../../components/ClientPageSize/ClientPageSize.js';
+import SitePage from '../../components/SitePage';
 
 class Client extends React.Component {
     render() {
 
         return (
-            <ClientPageSize>
-               <p> I AM THE CLIENT PAGE, YOU FUCK!</p> 
-
-                <p>Now bow before my greatness and suck my toes!</p>
-            </ClientPageSize>
+            <SitePage />
         );
     }
 }
