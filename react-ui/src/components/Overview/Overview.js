57c13ff17840a849926a01da441e4005b9a1a2b4
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-31 17:01:04 -0500

    changed focus css

--- react-ui/src/components/Overview/Overview.js
+++ react-ui/src/components/Overview/Overview.js
@@ -6,7 +6,7 @@ import './Overview.css';
 const Overview = (props) => {
     return (
         <div>
-            
+            You're getting the hang of this! Keep it up, my dude!
         </div>
     );
 }
