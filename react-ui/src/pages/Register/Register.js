a6d10520f5622700bf00e7eb165cf7cbf2979c9f
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-02-01 01:59:52 -0500

    edited login and register page

--- react-ui/src/pages/Register/Register.js
+++ react-ui/src/pages/Register/Register.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import './Register.css';
+
+const loginImgs = [
+    {
+        src: "https://raw.githubusercontent.com/DC-Developer/the-final-pt/master/react-ui/src/imgs/login-hero.png"
+    },
+    {
+        src: "https://raw.githubusercontent.com/DC-Developer/the-final-pt/master/react-ui/src/imgs/logo.png"
+    }
+];
 
 class Register extends React.Component {
     render() {
 
+        var mainImg = loginImgs[0];
+        var icon = loginImgs[1];
+
         return (
-            <div>
-                REGISTER ME, I AM THE REGISTER PAGE!
+            <div className="Register-Alt">
+                <img src={mainImg.src} />
+
+                <div className="col-nested-grid">
+                    <div className="loginLogoDiv">
+                        <img src={icon.src} className="loginLogo"/>FIT CLIENT
+                    </div>
+                    <div className="oauth2">
+                        <h1>Register</h1>
+                        <button className="Rectangle-Copy">Facebook</button>
+                        <button className="Rectangle-Copy-2">Google</button>
+                        
+                        <div className="hrDiv">
+                            <hr /><p className="hrText">or</p><hr />
+                        </div>
+
+                        <form action="" className="regForm" method="post">
+                            <input type="text" placeholder="    Email" className="Rectangle-register" /><br />
+                            <input type="password" placeholder="    Password" className="Rectangle-register" /><br />
+                            <input type="submit" value="Register" className="Rectangle-Copy-3" />
+                        </form>
+                    </div>
+  
+                </div>
             </div>
         );
     }
