d824d04a6558d51b9f63c82618b3d5e5ed3133ef
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-02-04 23:57:52 -0500

    added component styling

--- react-ui/src/components/ClientDiv/ClientDiv.js
+++ react-ui/src/components/ClientDiv/ClientDiv.js
@@ -3,10 +3,12 @@ import React from 'react';
 
 const ClientDiv = (props) => {
     return (
-        <div>
-            {props.fullname}
+    <div>
+        <div className="client">
+            <div className="clientName">{props.fullname}</div> <div className="clientDate">11-14-2017</div> <a>Edit</a>
         </div>
+    </div>
     );
 }
 
-export default ClientDiv;
+export default ClientDiv;   
