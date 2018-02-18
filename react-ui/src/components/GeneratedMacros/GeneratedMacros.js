90bd449965893d49385336cffff4fa22754c3c08
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-02-17 23:52:04 -0500

    fixed mis-alignment due to differring elements

--- react-ui/src/components/GeneratedMacros/GeneratedMacros.js
+++ react-ui/src/components/GeneratedMacros/GeneratedMacros.js
@@ -15,16 +15,16 @@ const GeneratedMacros = (props) => {
 
         <div className="macro_info" >
             <h2 className="macTitle">Training Day</h2>
-            <div> <h3 className="macLabel" id="training_protein">Protein</h3> <div className="macData" id="mac_data">{props.generateTrainingProtein()}</div> </div>
-            <div> <h3 className="macLabel" id="training_carbs">Carbs</h3> <div className="macData" id="mac_data">{props.generateTrainingCarbs()}</div> </div>
-            <div> <h3 className="macLabel" id="training_fats">Fats</h3> <div className="macData" id="mac_data">{props.generateTrainingFats()}</div> </div>
+            <div> <h3 className="macLabel" >Protein</h3> <div className="macData" id="mac_data">{props.generateTrainingProtein()}</div> </div>
+            <div> <h3 className="macLabel" >Carbs</h3> <div className="macData" id="mac_data">{props.generateTrainingCarbs()}</div> </div>
+            <div> <h3 className="macLabel" >Fats</h3> <div className="macData" id="mac_data">{props.generateTrainingFats()}</div> </div>
         </div>
 
         <div className="macro_info2" >
             <h2 className="macTitle">Rest</h2>
-            <div> <h3 className="macLabel" id="rest_protein">Protein</h3> <h3 className="macData">{props.generateRestProtein()}</h3> </div>
-            <div> <h3 className="macLabel" id="rest_carbs">Carbs</h3> <h3 className="macData">{props.generateRestCarbs()}</h3> </div>
-            <div> <h3 className="macLabel" id="rest_fats">Fats</h3> <h3 className="macData">{props.generateRestFats()}</h3> </div>
+            <div> <h3 className="macLabel" >Protein</h3> <div className="macData">{props.generateRestProtein()}</div> </div>
+            <div> <h3 className="macLabel" >Carbs</h3> <div className="macData">{props.generateRestCarbs()}</div> </div>
+            <div> <h3 className="macLabel" >Fats</h3> <div className="macData">{props.generateRestFats()}</div> </div>
         </div>
     </div>
    );
