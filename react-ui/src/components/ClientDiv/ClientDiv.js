1618ece67010300df521e1422de74348b2e33f3c
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-02-26 22:05:02 -0500

    edited client edit button

--- react-ui/src/components/ClientDiv/ClientDiv.js
+++ react-ui/src/components/ClientDiv/ClientDiv.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 
 const ClientDiv = (props) => {
+    var style = {color: "#50e2c1"};
+
     return (
     <div>
         <div className="client">
-            <div className="clientName">{props.fullname}</div> <div className="clientDate">{props.date}</div> <a>Edit</a>
+            <div className="clientName">{props.fullname}</div> <div className="clientDate">{props.date}</div> <a style={style}>Edit</a>
         </div>
     </div>
     );
